b2b78e904bf99d82631dbffc178ad3e348cfe1aa
diosmosis <benakamoorthi@fastmail.fm>
2014-03-11 22:45:38 +0000

    Debugging travis failure.

--- Test/Fixtures/TrackAdvancedCampaigns.php
+++ Test/Fixtures/TrackAdvancedCampaigns.php
@@ -19,7 +19,7 @@ class TrackAdvancedCampaigns extends Fixture
     const THIS_PAGE_VIEW_IS_GOAL_CONVERSION = 'this is a goal conversion';
 
     public function setUp()
-    {echo "PLUGINS: ".print_r(\Piwik\Config::getInstance()->Plugins, true)."\n";
+    {echo "plugins: " .print_r(\Piwik\Plugin\Manager::getInstance()->getPluginsToLoadDuringTests(), true);
         $this->setUpWebsite();
 
         // Track one visitor, with returning visit with advanced campaign use cases
