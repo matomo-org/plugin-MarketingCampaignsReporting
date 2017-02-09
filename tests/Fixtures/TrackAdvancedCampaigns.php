0f90ca286e1a7ddd4ba3840ebdd22dce1e84752b
Jarosław Zywert <j.zywert@piwik.pro>
2017-02-09 09:34:10 +0100

    git signature update

--- tests/Fixtures/TrackAdvancedCampaigns.php
+++ tests/Fixtures/TrackAdvancedCampaigns.php
@@ -1,11 +1,9 @@
 <?php
 /**
- * Piwik PRO - cloud hosting and enterprise analytics consultancy
- * from the creators of Piwik.org
+ * Piwik PRO -  Premium functionality and enterprise-level support for Piwik Analytics
  *
  * @link http://piwik.pro
  * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
- *
  */
 
 namespace Piwik\Plugins\AdvancedCampaignReporting\tests\Fixtures;
