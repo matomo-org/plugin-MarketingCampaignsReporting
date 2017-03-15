c4ed920a858f45720d38673020fd4aabd421e6e3
sgiehl <stefan@piwik.org>
2017-03-15 17:57:43 +0100

    ensure to force a new visit if any new campaign parameter is detected

--- Columns/CampaignName.php
+++ Columns/CampaignName.php
@@ -8,8 +8,15 @@
  */
 namespace Piwik\Plugins\MarketingCampaignsReporting\Columns;
 
+use Piwik\Common;
+use Piwik\Container\StaticContainer;
 use Piwik\Piwik;
+use Piwik\Plugins\MarketingCampaignsReporting\MarketingCampaignsReporting;
 use Piwik\Plugins\MarketingCampaignsReporting\Segment;
+use Piwik\Tracker\Action;
+use Piwik\Tracker\Request;
+use Piwik\Tracker\TrackerConfig;
+use Piwik\Tracker\Visitor;
 
 class CampaignName extends Base
 {
@@ -27,5 +34,40 @@ class CampaignName extends Base
     public function getName()
     {
         return Piwik::translate('MarketingCampaignsReporting_Name');
+    }
+
+    /**
+     * Force a new visit if any new campaign parameters are detected
+     *
+     * @param Request     $request
+     * @param Visitor     $visitor
+     * @param Action|null $action
+     * @return bool
+     */
+    public function shouldForceNewVisit(Request $request, Visitor $visitor, Action $action = null)
+    {
+        if (TrackerConfig::getConfigValue('create_new_visit_when_campaign_changes') != 1) {
+            return false;
+        }
+
+        $campaignDetector   = StaticContainer::get('advanced_campaign_reporting.campaign_detector');
+        $campaignParameters = MarketingCampaignsReporting::getCampaignParameters();
+
+        $campaignDimensions = $campaignDetector->detectCampaignFromRequest(
+            $request,
+            $campaignParameters
+        );
+
+        if (empty($campaignDimensions)) {
+            return false;
+        }
+
+        foreach ($campaignDimensions as $dimension => $value) {
+            if (Common::mb_strtolower($visitor->getVisitorColumn($dimension)) != Common::mb_strtolower($value)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
