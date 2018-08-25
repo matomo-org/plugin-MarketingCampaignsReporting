3e54f160d70485e45cf91246116dd7d8b47e83f5
sgiehl <stefan@piwik.org>
2018-08-25 18:19:32 +0200

    Converts UI tests to use Chrome Headless

--- tests/UI/Reports_spec.js
+++ tests/UI/Reports_spec.js
@@ -13,9 +13,8 @@ describe("MarketingCampaignsReporting_Reports", function () {
 
     var url = "?module=CoreHome&action=index&idSite=1&date=2013-01-23&period=month#?category=Referrers_Referrers&subcategory=Referrers_Campaigns";
 
-    it("should load correctly when user goes to normal campaigns page", function (done) {
-        expect.screenshot("loaded").to.be.captureSelector('.pageWrap,.expandDataTableFooterDrawer', function (page) {
-            page.load(url);
-        }, done);
+    it("should load correctly when user goes to normal campaigns page", async function () {
+        await page.goto(url);
+        expect(await page.screenshotSelector('.pageWrap,.expandDataTableFooterDrawer')).to.matchImage('loaded');
     });
 });
